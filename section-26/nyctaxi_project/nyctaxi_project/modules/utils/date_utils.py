1be36da256fadb5d6529e901ff913ae247808a52
Kaloyan Marinov <happy-2025-04-06-14-33@protonmail.com>
2026-02-15 18:42:39 +0100

    refactor `section-26/nyctaxi_project/nyctaxi_project/modules/utils/date_utils.py`

--- section-26/nyctaxi_project/nyctaxi_project/modules/utils/date_utils.py
+++ section-26/nyctaxi_project/nyctaxi_project/modules/utils/date_utils.py
@@ -6,7 +6,8 @@ def get_target_yyyymm(months_ago: int = 2) -> str:
     """
     Return the year-month string (yyyy-MM) for the given number of months ago.
     """
-    target_date = dt.date.today() - relativedelta(months=months_ago)
+    today = dt.date.today()
+    target_date = today - relativedelta(months=months_ago)
     return target_date.strftime("%Y-%m")
 
 
@@ -14,5 +15,6 @@ def get_month_start_n_months_ago(months_ago: int = 2) -> dt.date:
     """
     Return the date representing the 1st day of the month, 'n' months ago.
     """
-    first_day_in_curr_month = dt.date.today().replace(day=1)
+    today = dt.date.today()
+    first_day_in_curr_month = today.replace(day=1)
     return first_day_in_curr_month - relativedelta(months=months_ago)
