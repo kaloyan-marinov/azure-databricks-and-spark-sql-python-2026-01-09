f191e39d940ab922ce6e625d4802f3a0a7031c90
Kaloyan Marinov <happy-2025-04-06-14-33@protonmail.com>
2026-02-15 18:14:55 +0100

    add `section-26/nyctaxi_project/nyctaxi_project/modules/file_downloader.py`

---
+++ section-26/nyctaxi_project/nyctaxi_project/modules/file_downloader.py
@@ -0,0 +1,16 @@
+import urllib.request
+import os
+import shutil
+
+
+def download_file(url: str, dir_path: str, local_path: str) -> None:
+    os.makedirs(
+        os.path.dirname(dir_path),
+        exists_ok=True,
+    )
+
+    with urllib.request.urlopen(url) as response, open(local_path, "wb") as out_file:
+        shutil.copyfileobj(
+            response,
+            out_file,
+        )
