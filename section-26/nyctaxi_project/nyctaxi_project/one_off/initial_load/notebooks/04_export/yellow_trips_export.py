ee0e378737c801c6ad594315f7e408494d404164
Kaloyan Marinov <happy-2025-04-06-14-33@protonmail.com>
2026-03-08 12:32:12 +0100

    WORK ON "growing `section-26/nyctaxi_project/` into" an implemenation of «Part 4 of the Project»
    specifically, this commit pertains to `section-28/185-*` & cleans code by reducing duplication

--- section-26/nyctaxi_project/nyctaxi_project/one_off/initial_load/notebooks/04_export/yellow_trips_export.py
+++ section-26/nyctaxi_project/nyctaxi_project/one_off/initial_load/notebooks/04_export/yellow_trips_export.py
@@ -5,9 +5,22 @@ from pyspark.sql.functions import (
 
 # COMMAND ----------
 
+url_for_external_location = "abfss://nyctaxi-yellow@nyctaxistorage17.dfs.core.windows.net/"
+target_folder = 'yellow_trips_export/'
+
+catalog = 'nyctaxi'
+
+source_schema = '02_silver'
+source_table = 'yellow_trips_enriched'
+
+target_schema = '04_export'
+target_table = 'yellow_trips_export'
+
+# COMMAND ----------
+
 # Add a column called `year_month`.
 
-df = spark.read.table('nyctaxi.02_silver.yellow_trips_enriched')
+df = spark.read.table(f'{catalog}.{source_schema}.{source_table}')
 
 df = df.withColumn(
     'year_month',
@@ -22,8 +35,8 @@ df = df.withColumn(
 # Write the `DataFrame` to the specified «external table».
 
 df.write.\
-    option('path', 'abfss://nyctaxi-yellow@nyctaxistorage17.dfs.core.windows.net/yellow_trips_export/').\
+    option('path', f'{url_for_external_location}{target_folder}').\
     format('json').\
     mode('overwrite').\
     partitionBy('vendor', 'year_month').\
-    saveAsTable('nyctaxi.04_export.yellow_trips_export')
+    saveAsTable(f'{catalog}.{target_schema}.{target_table}')
