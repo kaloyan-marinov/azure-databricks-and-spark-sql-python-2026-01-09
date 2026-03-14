8a32dbfff6010f6643b7eda218eb2214cab7ddd4
Kaloyan Marinov <happy-2025-04-06-14-33@protonmail.com>
2026-03-14 18:02:58 +0100

    192-* = `cp section-26/nyctaxi_project/nyctaxi_project/transformations/notebooks/04_export/yellow_trips_export.py 'section-29/2026-03-14 18:00:46 Streaming Simulator Notebook.py'`

---
+++ section-29/2026-03-14 18:00:46 Streaming Simulator Notebook.py
@@ -0,0 +1,74 @@
+# Databricks notebook source
+import os
+import sys
+
+project_root = os.path.join(
+    os.getcwd(),
+    '..',
+    '..',
+    '..',
+)
+project_root = os.path.abspath(project_root)
+
+if project_root not in sys.path:
+    sys.path.append(project_root)
+
+# COMMAND ----------
+
+from pyspark.sql.functions import (
+    date_format,
+)
+
+from modules.utils.date_utils import get_month_start_n_months_ago
+
+# COMMAND ----------
+
+url_for_external_location = "abfss://nyctaxi-yellow@nyctaxistorage17.dfs.core.windows.net/"
+target_folder = 'yellow_trips_export/'
+
+catalog = 'nyctaxi'
+
+source_schema = '02_silver'
+source_table = 'yellow_trips_enriched'
+
+target_schema = '04_export'
+target_table = 'yellow_trips_export'
+
+# COMMAND ----------
+
+two_months_ago_start = get_month_start_n_months_ago(months_ago=2)
+
+# COMMAND ----------
+
+# Read the table
+# and
+# filter its contents appropriately.
+
+df = spark.read.table(f'{catalog}.{source_schema}.{source_table}')
+
+df = df.filter(f"tpep_pickup_datetime > '{two_months_ago_start}'")
+
+# Add a column called `year_month`.
+
+df = df.withColumn(
+    'year_month',
+    date_format(
+        'tpep_pickup_datetime',
+        'yyyy-MM',
+    ),
+)
+
+# COMMAND ----------
+
+# Append the `DataFrame`'s contents to the specified «external table».
+
+df.write.saveAsTable(
+    f'{catalog}.{target_schema}.{target_table}',
+    path=f'{url_for_external_location}{target_folder}',
+    mode='append',
+    format='json',
+    partitionBy=[
+        'vendor',
+        'year_month',
+    ],
+)
