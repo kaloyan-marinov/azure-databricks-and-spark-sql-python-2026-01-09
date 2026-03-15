2eed3fb39e0008991ce1cdc524ab93016809571b
Kaloyan Marinov <happy-2025-04-06-14-33@protonmail.com>
2026-03-15 20:39:27 +0100

    clean code = capitalize SQL keywords

--- section-30/206-repo-for-this-section-s-project/Bridge-Monitoring-Streaming-Pipeline-with-Lakeflow-Declarative-Pipelines/queries.sql
+++ section-30/206-repo-for-this-section-s-project/Bridge-Monitoring-Streaming-Pipeline-with-Lakeflow-Declarative-Pipelines/queries.sql
@@ -1,17 +1,17 @@
 --landing
-select * from delta.`/Volumes/bridge_monitoring/00_landing/streaming/bridge_temperature/`;
-select * from delta.`/Volumes/bridge_monitoring/00_landing/streaming/bridge_tilt/`;
-select * from delta.`/Volumes/bridge_monitoring/00_landing/streaming/bridge_vibrations/`;
+SELECT * FROM delta.`/Volumes/bridge_monitoring/00_landing/streaming/bridge_temperature/`;
+SELECT * FROM delta.`/Volumes/bridge_monitoring/00_landing/streaming/bridge_tilt/`;
+SELECT * FROM delta.`/Volumes/bridge_monitoring/00_landing/streaming/bridge_vibrations/`;
 
 --bronze
-select * from bridge_monitoring.`01_bronze`.bridge_temperature order by event_time desc;
-select * from bridge_monitoring.`01_bronze`.bridge_tilt order by event_time desc;
-select * from bridge_monitoring.`01_bronze`.bridge_vibration order by event_time desc;
+SELECT * FROM bridge_monitoring.`01_bronze`.bridge_temperature ORDER BY event_time DESC;
+SELECT * FROM bridge_monitoring.`01_bronze`.bridge_tilt ORDER BY event_time DESC;
+SELECT * FROM bridge_monitoring.`01_bronze`.bridge_vibration ORDER BY event_time DESC;
 
 --silver
-select * from bridge_monitoring.`02_silver`.bridge_temperature order by event_time desc;
-select * from bridge_monitoring.`02_silver`.bridge_tilt order by event_time desc;
-select * from bridge_monitoring.`02_silver`.bridge_vibration order by event_time desc;
+SELECT * FROM bridge_monitoring.`02_silver`.bridge_temperature ORDER BY event_time DESC;
+SELECT * FROM bridge_monitoring.`02_silver`.bridge_tilt ORDER BY event_time DESC;
+SELECT * FROM bridge_monitoring.`02_silver`.bridge_vibration ORDER BY event_time DESC;
 
 --gold
-select * from bridge_monitoring.`03_gold`.bridge_metrics order by window_start desc;
+SELECT * FROM bridge_monitoring.`03_gold`.bridge_metrics ORDER BY window_start DESC;
