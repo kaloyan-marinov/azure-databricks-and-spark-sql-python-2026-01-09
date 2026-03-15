618b4014106012df2a85b71b61ffc59b0561a832
Kaloyan Marinov <happy-2025-04-06-14-33@protonmail.com>
2026-03-15 20:41:20 +0100

    clean code = increase readability by splitting each SQL statement across several lines

--- section-30/206-repo-for-this-section-s-project/Bridge-Monitoring-Streaming-Pipeline-with-Lakeflow-Declarative-Pipelines/queries.sql
+++ section-30/206-repo-for-this-section-s-project/Bridge-Monitoring-Streaming-Pipeline-with-Lakeflow-Declarative-Pipelines/queries.sql
@@ -1,17 +1,83 @@
 --landing
-SELECT * FROM delta.`/Volumes/bridge_monitoring/00_landing/streaming/bridge_temperature/`;
-SELECT * FROM delta.`/Volumes/bridge_monitoring/00_landing/streaming/bridge_tilt/`;
-SELECT * FROM delta.`/Volumes/bridge_monitoring/00_landing/streaming/bridge_vibrations/`;
+SELECT
+    *
+FROM
+    delta.`/Volumes/bridge_monitoring/00_landing/streaming/bridge_temperature/`
+;
+
+SELECT
+    *
+FROM
+    delta.`/Volumes/bridge_monitoring/00_landing/streaming/bridge_tilt/`
+;
+
+SELECT
+    *
+FROM
+    delta.`/Volumes/bridge_monitoring/00_landing/streaming/bridge_vibrations/`
+;
+
+
 
 --bronze
-SELECT * FROM bridge_monitoring.`01_bronze`.bridge_temperature ORDER BY event_time DESC;
-SELECT * FROM bridge_monitoring.`01_bronze`.bridge_tilt ORDER BY event_time DESC;
-SELECT * FROM bridge_monitoring.`01_bronze`.bridge_vibration ORDER BY event_time DESC;
+SELECT
+    *
+FROM
+    bridge_monitoring.`01_bronze`.bridge_temperature
+ORDER BY
+    event_time DESC
+;
+
+SELECT
+    *
+FROM
+    bridge_monitoring.`01_bronze`.bridge_tilt
+ORDER BY
+    event_time DESC
+;
+
+SELECT
+    *
+FROM
+    bridge_monitoring.`01_bronze`.bridge_vibration
+ORDER BY
+    event_time DESC
+;
+
+
 
 --silver
-SELECT * FROM bridge_monitoring.`02_silver`.bridge_temperature ORDER BY event_time DESC;
-SELECT * FROM bridge_monitoring.`02_silver`.bridge_tilt ORDER BY event_time DESC;
-SELECT * FROM bridge_monitoring.`02_silver`.bridge_vibration ORDER BY event_time DESC;
+SELECT
+    *
+FROM
+    bridge_monitoring.`02_silver`.bridge_temperature
+ORDER BY
+    event_time DESC
+;
+
+SELECT
+    *
+FROM
+    bridge_monitoring.`02_silver`.bridge_tilt
+ORDER BY
+    event_time DESC
+;
+
+SELECT
+    *
+FROM
+    bridge_monitoring.`02_silver`.bridge_vibration
+ORDER BY
+    event_time DESC
+;
+
+
 
 --gold
-SELECT * FROM bridge_monitoring.`03_gold`.bridge_metrics ORDER BY window_start DESC;
+SELECT
+    *
+FROM
+    bridge_monitoring.`03_gold`.bridge_metrics
+ORDER BY
+    window_start DESC
+;
