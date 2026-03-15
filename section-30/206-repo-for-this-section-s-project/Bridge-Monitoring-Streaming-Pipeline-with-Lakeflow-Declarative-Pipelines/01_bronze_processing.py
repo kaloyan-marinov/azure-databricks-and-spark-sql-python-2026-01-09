8218a3ba204ddfb224982c08e56297dd537cb706
Kaloyan Marinov <happy-2025-04-06-14-33@protonmail.com>
2026-03-15 20:52:47 +0100

    clean code = reduce duplication

--- section-30/206-repo-for-this-section-s-project/Bridge-Monitoring-Streaming-Pipeline-with-Lakeflow-Declarative-Pipelines/01_bronze_processing.py
+++ section-30/206-repo-for-this-section-s-project/Bridge-Monitoring-Streaming-Pipeline-with-Lakeflow-Declarative-Pipelines/01_bronze_processing.py
@@ -6,34 +6,36 @@
 
 import dlt
 
-# Bronze: raw ingestion of three delta streams
+
+# Common settings
+CATALOG = "bridge_monitoring"
+
+SCHEMA_00_LANDING = "00_landing"
+VOLUME = "streaming"
+_PARENT_PATH = f"/Volumes/{CATALOG}/{SCHEMA_00_LANDING}/{VOLUME}"
+
+SCHEMA_01_BRONZE = "01_bronze"
 
 
 @dlt.table(
-    name="01_bronze.bridge_temperature",
+    name=f"{SCHEMA_01_BRONZE}.bridge_temperature",
     comment="Raw temperature readings",
 )
 def bronze_bridge_temperature():
-    return spark.readStream.format("delta").load(
-        "/Volumes/bridge_monitoring/00_landing/streaming/bridge_temperature"
-    )
+    return spark.readStream.format("delta").load(f"{_PARENT_PATH}/bridge_temperature")
 
 
 @dlt.table(
-    name="01_bronze.bridge_vibration",
+    name=f"{SCHEMA_01_BRONZE}.bridge_vibration",
     comment="Raw vibration readings",
 )
 def bronze_bridge_vibration():
-    return spark.readStream.format("delta").load(
-        "/Volumes/bridge_monitoring/00_landing/streaming/bridge_vibration"
-    )
+    return spark.readStream.format("delta").load(f"{_PARENT_PATH}/bridge_vibration")
 
 
 @dlt.table(
-    name="01_bronze.bridge_tilt",
+    name=f"{SCHEMA_01_BRONZE}.bridge_tilt",
     comment="Raw tilt‐angle readings",
 )
 def bronze_bridge_tilt():
-    return spark.readStream.format("delta").load(
-        "/Volumes/bridge_monitoring/00_landing/streaming/bridge_tilt"
-    )
+    return spark.readStream.format("delta").load(f"{_PARENT_PATH}/bridge_tilt")
