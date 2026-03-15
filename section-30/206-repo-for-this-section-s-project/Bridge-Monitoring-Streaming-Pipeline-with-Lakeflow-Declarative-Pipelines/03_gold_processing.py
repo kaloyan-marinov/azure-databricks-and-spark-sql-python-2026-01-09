b090dd82891ca3530cc3baf0b884f18d9a1031d8
Kaloyan Marinov <happy-2025-04-06-14-33@protonmail.com>
2026-03-15 21:49:32 +0100

    clean code = extract `WIDTH_OF_TIMESTAMP_WINDOW` and `THRESHOLD_OF_LATENESS`

--- section-30/206-repo-for-this-section-s-project/Bridge-Monitoring-Streaming-Pipeline-with-Lakeflow-Declarative-Pipelines/03_gold_processing.py
+++ section-30/206-repo-for-this-section-s-project/Bridge-Monitoring-Streaming-Pipeline-with-Lakeflow-Declarative-Pipelines/03_gold_processing.py
@@ -2,7 +2,7 @@
 
 # COMMAND ----------
 
-# Gold: compute aggregations of the silver-layer data records over 10-minute windows
+# Gold: compute aggregations of the silver-layer data records over 10-minute tumbling windows
 
 import dlt
 from pyspark.sql.functions import col, window, max, avg, round
@@ -13,9 +13,11 @@ BRIDGE_TEMPERATURE = "bridge_temperature"
 BRIDGE_VIBRATION = "bridge_vibration"
 BRIDGE_TILT = "bridge_tilt"
 
-SCHEMA_03_GOLD = '03_gold'
-BRIDGE_METRICS = 'bridge_metrics'
+WIDTH_OF_TIMESTAMP_WINDOW = "10 minutes"
+THRESHOLD_OF_LATENESS = "2 minutes"
 
+SCHEMA_03_GOLD = "03_gold"
+BRIDGE_METRICS = "bridge_metrics"
 
 
 @dlt.table(
@@ -23,24 +25,24 @@ BRIDGE_METRICS = 'bridge_metrics'
     comment="10-min avg temperature, max vibration & max tilt per bridge with window start/end",
 )
 def bridge_metrics():
-    # Apply a 2-minute watermark to bound late data for stateful ops
+    # Read data streams from the preceding layer.
     temp = dlt.read_stream(f"{SCHEMA_02_SILVER}.{BRIDGE_TEMPERATURE}").withWatermark(
         "event_time",
-        "2 minutes",
+        THRESHOLD_OF_LATENESS,
     )
     vib = dlt.read_stream(f"{SCHEMA_02_SILVER}.{BRIDGE_VIBRATION}").withWatermark(
         "event_time",
-        "2 minutes",
+        THRESHOLD_OF_LATENESS,
     )
     tilt = dlt.read_stream(f"{SCHEMA_02_SILVER}.{BRIDGE_TILT}").withWatermark(
         "event_time",
-        "2 minutes",
+        THRESHOLD_OF_LATENESS,
     )
 
     # Compute 10-minute tumbling average temperature, retaining metadata
     temp_agg = (
         temp.groupBy(
-            window("event_time", "10 minutes"),
+            window("event_time", WIDTH_OF_TIMESTAMP_WINDOW),
             col("bridge_id"),
             col("name"),
             col("location"),
@@ -58,7 +60,7 @@ def bridge_metrics():
 
     # Compute 10-minute max vibration per bridge
     vib_agg = (
-        vib.groupBy(window("event_time", "10 minutes"), col("bridge_id"))
+        vib.groupBy(window("event_time", WIDTH_OF_TIMESTAMP_WINDOW), col("bridge_id"))
         .agg(max("vibration").alias("max_vibration"))
         .select(
             col("bridge_id"),
@@ -70,7 +72,7 @@ def bridge_metrics():
 
     # Compute 10-minute max tilt angle per bridge
     tilt_agg = (
-        tilt.groupBy(window("event_time", "10 minutes"), col("bridge_id"))
+        tilt.groupBy(window("event_time", WIDTH_OF_TIMESTAMP_WINDOW), col("bridge_id"))
         .agg(max("tilt_angle").alias("max_tilt_angle"))
         .select(
             col("bridge_id"),
